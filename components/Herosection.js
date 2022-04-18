d42850710830d2dec04aa014883312ad4a4811c8
BuildTools <unconfigured@null.spigotmc.org>
2022-04-18 18:20:36 +0200

    Updated footer.js

---
+++ components/Herosection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Herosection() {
+  return (
+    <h1 className='md:text-8xl text-4xl font-bold text-center text-gravy-900'>
+        Responsive <span className='text-blue-700'> Footer 
+        </span> using{" "} <span className='text-blue-700'>NextJS</span> &{" "} 
+        <span className='text-blue-700'>TailwindCSS</span>
+    </h1>
+  )
+}
+
+export default Herosection
